3e4b0c1344c2872fe5d4b5f39533fa1be86d9e5c
Josh Akin <jdakin2@illinois.edu>
2022-01-14 20:43:43 -0600

    Releasing Labs 1 and 2

---
+++ lab2/lab2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>  
+#include <math.h>  
+
+int main()  
+{  
+    int a;
+
+    for (a = 256; a > 1; a = sqrt(a))  
+    {  
+        printf("%d\n", a);  
+    }  
+
+    printf("%d\n", 1);  
+
+    return 0;
+}  
+
