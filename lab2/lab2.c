4ba61d27ecdb6a03ef07e72bc351e4f70f07ce5e
Bill Yang <hy38@illinois.edu>
2022-01-31 13:02:34 -0600

    finished lab 2

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -1,17 +1,10 @@
-#include <stdio.h>  
-#include <math.h>  
-
-int main()  
-{  
-    int a;
-
-    for (a = 256; a > 1; a = sqrt(a))  
-    {  
-        printf("%d\n", a);  
-    }  
-
-    printf("%d\n", 1);  
-
-    return 0;
-}  
-
+#include <stdio.h>
+#include <math.h>
+int main(){   
+    int a = 256;
+    while(a > 1){
+        printf("%d\n",a);
+        a = sqrt(a);
+    }
+    printf("%d\n", 1);    return 0;
+}
