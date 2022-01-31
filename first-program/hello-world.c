7beddc69cb316de00d638c4e49fdbcef7f25232f
Bill Yang <hy38@illinois.edu>
2022-01-31 12:46:30 -0600

    Submitted lab2.txt

---
+++ first-program/hello-world.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#define MESSAGE "Hello, x86 world!\n"
+int main()
+{
+    printf(MESSAGE);
+    return 0;
+}
---
+++ num_comps.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int val1 = -11;
+    int val2 = 3;
+    int res;
+     res = val1 % val2;
+     printf("%d\n", res);
+     return 0;
+}
