3eb9449cc2b8867124cdd00fe868cdbb681dc45f
Bhowmik, Ujjal Kumar <ubhowmik@illinois.edu>
2022-04-15 17:08:21 -0500

    Update lab11.asm

--- lab11/lab11.asm
+++ lab11/lab11.asm
@@ -2,7 +2,7 @@
 ;   different numbers (4!, 6!, etc.) by changing the value of the first memory
 ;   location at the bottom of the code. It is currently set up to calculate
 ;   5!. The program does not account for zero or negative numbers as input.
-
+ 
 ; This program primarily uses registers in the following manner:
 ; R0 contains 0 (registers contain zero after reset)
 ; R1 contains multiplication result (6x5 = 30, 30x4 = 120, etc)
@@ -10,16 +10,16 @@
 ; R3 contains counter for outer loop
 ; R4 contains counter for inner loop
 ; R5 contains current sum
-
+ 
             x3000
-
+ 
             LD    R1,INPUT            ; R1 contains input number
             LD    R2,x3010            ; R2 contains -1
             ADD   R3,R1,R2            ; R3 contains input number -1
             ADD   R3,,R3,R2           ; R3 contains input number -2
                                       ;   (initializes outer count)
 OUTERLOOP   ADD   R4,R0,R3            ; Copy outer count into inner count
-
+ 
 ; This loop multiplies via addition (6x5 = 6+6+6+6+6 = 30,
 ;   30x4 = 30+30+30+30 = 120, etc)
 INNERLOOP   ADD   R5,R5,R1            ; Increment sum
@@ -31,12 +31,11 @@ INNERLOOP   ADD   R5,R5,R1            ; Increment sum
             ADD   R3,R3,R2            ; Decrement outer count
             BRpz  OUTERLOOP           ; Branch to outer loop if outer count
                                       ;   is positive or zero
-
-            ST   R1,x3011             ; This address contains X!          
+ 
+            STI  R1,  xxxx            ; 5! is stored in the memory location (x30FF)
             TRAP x25
-
+ 
 INPUT      .FILL  x0005               ; Input for X!, in this case X = 5
-           .FILL  x0000
+           .FILL  x0000               ; Can be used to initialize registers
            .FILL  xFFFF               ; 2's complement of 1 (i.e. -1)
-           .FILL  x0000               ; At program completion, the result is
-                                      ;   stored here
+RESULT     .FILL  x30FF               ; At program completion, the result is stored at x30FF
